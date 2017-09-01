df712fec56808d7a536680840ac63f402681a82d
Michael Chen <michael.chen.cse@gmail.com>
2017-09-01 02:44:41 -0400

    fix production middleware for redux

--- src/store.js
+++ src/store.js
@@ -6,12 +6,15 @@ import {persistStore, autoRehydrate} from 'redux-persist'
 
 import rootReducer from './Reducers';
 
+const middlewares = [];
+if(process.env.NODE_ENV === 'development') middlewares.push(createLogger({collapsed: true}));
+middlewares.push(thunkMiddleware);
+
 const store = createStore(
   rootReducer,
   composeWithDevTools(
     applyMiddleware(
-      thunkMiddleware,
-      createLogger({collapsed: true})
+      ...middlewares
     ),
     autoRehydrate()
   )
